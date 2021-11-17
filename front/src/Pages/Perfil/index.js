ee0dc49cd3daa99633747219eb2df5f5d632f4d7
Gustavo <gustavofloripa98@gmail.com>
2021-11-16 21:40:42 -0300

    alteracao tela perfil

--- front/src/Pages/Perfil/index.js
+++ front/src/Pages/Perfil/index.js
@@ -1,40 +1,49 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { Feather } from "@expo/vector-icons";
 
-import {
-    Container,
-    InfoView,
-    Nome,
-    Email,
-    Buttonfunc
-} from './style';
+import * as C from './style';
 
 const Perfil = () => {
 
-    return(
+    return (
 
-        <Container>
-            <InfoView>
-                <Text>Vai ter um icone</Text>
-                <Nome>Fulaninha</Nome>
-                <Email>exemplo@gmail.com</Email>
-            </InfoView>
-            <Buttonfunc>
-                <Text>Trocar apelido</Text>
-            </Buttonfunc>
-            <Buttonfunc>
-                <Text>Trocar senha</Text>
-            </Buttonfunc>
-            <Buttonfunc>
-                <Text>Favoritos</Text>
-            </Buttonfunc>
-            <Buttonfunc>
-                <Text>Sair</Text>
-            </Buttonfunc>
-            <Buttonfunc>
-                <Text style={{color: 'red'}}>Excluir conta</Text>
-            </Buttonfunc>
-        </Container>
+        <C.Container>
+            <C.ContainerHeader>
+                <C.ButtonLeft>
+                    <Feather name='chevron-left' size={24} color='black' />
+                </C.ButtonLeft>
+                <C.Title>Meu Perfil</C.Title>
+                <C.ButtonRight>
+                    <Feather name='more-horizontal' size={24} color='black' />
+                </C.ButtonRight>
+            </C.ContainerHeader>
+            <C.Info>
+
+                <C.Name>Exemplo</C.Name>
+                <C.Email>exemplo@exemplo.com</C.Email>
+            </C.Info>
+
+            <C.MenuBottons>
+                <C.Buttons>
+                    <C.TextButtons>Trocar Apelido</C.TextButtons>
+                    <Feather name='chevron-right' size={24} color='black' />
+                </C.Buttons>
+                <C.Buttons>
+                    <C.TextButtons>Trocar Senha</C.TextButtons>
+                    <Feather name='chevron-right' size={24} color='black' />
+                </C.Buttons>
+                <C.Buttons>
+                    <C.TextButtons>Favoritos</C.TextButtons>
+                    <Feather name='chevron-right' size={24} color='black' />
+                </C.Buttons>
+                <C.Buttons>
+                    <C.TextButtons>Sair</C.TextButtons> 
+                </C.Buttons>
+                <C.Buttons>
+                    <C.TextButtons style={{color: 'red'}}>Excluir Conta</C.TextButtons> 
+                </C.Buttons>
+            </C.MenuBottons>
+        </C.Container>
     )
 
 }
--- front/src/Pages/Perfil/style.js
+++ front/src/Pages/Perfil/style.js
@@ -2,42 +2,60 @@ import styled from "styled-components/native";
 
 export const Container = styled.SafeAreaView`
     flex: 1px;
-    margin-left: 10px;
+    margin-top: 50px;
+    background-color: #fff;
 `;
 
-export const InfoView = styled.View`
-    background-color: #0431B4;
-    border-radius: 15px;
-    padding: 70px;
-    width: 80%;
-    margin-top: 10px;
-    margin-left: 30px;
-    margin-bottom: 100px;
+export const ContainerHeader = styled.View`
+    justify-content: space-between;
+    margin: 0px 10px;
+    align-items: center;
+    flex-direction: row;
 `;
 
-export const Nome = styled.Text`
+export const Title = styled.Text`
+    font-size: 20px;
+`;
+
+export const ButtonLeft = styled.TouchableOpacity``;
+
+export const ButtonRight = styled.TouchableOpacity``;
+
+export const Info = styled.View`
+    background-color: #0D47A1;
+    justify-content: center;
+    align-items: center;
+    width: 85%;
+    margin: 40px 28px;
+    padding: 90px;
+    border-radius: 20px;
+    
+`;
+export const Name = styled.Text`
     color: #fff;
     font-weight: bold;
-    margin-top: 15px;  
+    font-size: 18px;
+    margin-bottom: 15px;
+    
 `;
 
 export const Email = styled.Text`
     color: #fff;
-    margin-top: 10px;
-    font-size: 10px;
+    font-size: 13px;
+`;
+
+export const MenuBottons = styled.View`
+`;
+
+export const Buttons = styled.TouchableOpacity`
+    justify-content: space-between;
+    flex-direction: row;
+    margin: 5px 28px;
+    border-bottom-width: 1px;
 
 `;
 
-export const Buttonfunc = styled.TouchableOpacity`
-    border-width: 1px;
-    width: 90%;
-    margin-bottom: 10px;
+export const TextButtons = styled.Text`
+    font-weight: bold;
+    font-size: 15px;
 `;
-
-
-
-
-
-
-
-
