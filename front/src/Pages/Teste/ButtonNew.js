7a8c9df3e1bab9f923f483ca56c732ea81c08d87
Gustavo <gustavofloripa98@gmail.com>
2021-11-24 21:38:43 -0300

    TabBars

--- front/src/Pages/Teste/ButtonNew.js
+++ front/src/Pages/Teste/ButtonNew.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-
-import { Entypo } from "@expo/vector-icons"
-import styled from 'styled-components/native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
 const ButtonNew = ({ size, focused }) => {
-
-    return(
-
-        <View style={[styles.container, { backgroundColor: focused ? "#3eccf5" : "#6fdfff"}]}>
-            <Entypo name="plus" color={focused ? "#FFF" : "#F8f8f8"} size={size} />
-        </View>
-    )
-
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: focused ? "#3eccf5" : "#6fdfff" },
+      ]}
+    >
+      <Entypo name="plus" color={focused ? "#FFF" : "#F8f8f8"} size={size} />
+    </View>
+  );
 };
 
-//"#3eccf5" : "#6fdfff"
-
 const styles = StyleSheet.create({
-    container: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        backgroundColor: "#3eccf5",
-        alignItems: "center",
-        justifyContent: "center",
-        marginBottom: 20,
-    }
+  container: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: "#3eccf5",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
 });
 
 export default ButtonNew;
