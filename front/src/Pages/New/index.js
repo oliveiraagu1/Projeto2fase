ea7ccb11f77af785b79292e2a4d03a29689a3b5b
Gustavo <gustavofloripa98@gmail.com>
2021-11-29 21:47:30 -0300

    incluindo tela cadastro regiao

--- front/src/Pages/New/index.js
+++ front/src/Pages/New/index.js
@@ -1,36 +1,60 @@
-import React, { useEffect } from "react";
-import { View, Text, } from "react-native";
-
-import apiGoogle, { YOUR_API_KEY } from "../../Services/Api/api";
-import axios from "axios";
-
+import React from "react";
+import { KeyboardAvoidingView } from "react-native";
 import Header from "../../Components/Header";
+import { Feather } from "@expo/vector-icons"
+import * as C from "./style";
+
+// import apiGoogle, { YOUR_API_KEY } from "../../Services/Api/api";
+// import axios from "axios";
 
 const NewInfos = () => {
+  // useEffect(()=>{
+  //   const user = async () => {
 
-  useEffect(()=>{
-    const user = async () => {
-   
-      // try{
-      //   const result = await apiGoogle.get(
-      //     `fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&key=${YOUR_API_KEY}`
-      //   );
-      //   console.log(JSON.stringify(result.data));
-      //   alert("suceeos")
-      // }catch(error){
-      //   console.log(error);
-      //   alert('error')
-      // }
-    };
-    user();
-  },[])
-  
-
+  //     // try{
+  //     //   const result = await apiGoogle.get(
+  //     //     `fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&key=${YOUR_API_KEY}`
+  //     //   );
+  //     //   console.log(JSON.stringify(result.data));
+  //     //   alert("suceeos")
+  //     // }catch(error){
+  //     //   console.log(error);
+  //     //   alert('error')
+  //     // }
+  //   };
+  //   user();
+  // },[])
 
   return (
-    <View>
-      <Header />
-    </View>
+    <C.Container>
+      <KeyboardAvoidingView behavior="padding">
+        <Header />
+        <C.SubHeader>
+          <C.Title>CADASTRANDO REGIÃO</C.Title>
+          <C.SubTitle>
+            Conhece algum ponto histórico em Florianópolis e quer cadastrar?
+          </C.SubTitle>
+        </C.SubHeader>
+        <C.Info>
+          <C.InfoTitle>Nome do Local:</C.InfoTitle>
+          <C.InputMenor maxLength={50} />
+          <C.InfoTitle>Descrição:</C.InfoTitle>
+          <C.InputDesc multiline={true} maxLength={200} />
+
+          <C.ViewImage>
+          <C.InfoTitle>Imagem:</C.InfoTitle>
+          <C.ButtonImage>
+          <Feather name="paperclip" size={24} color="black" />
+          </C.ButtonImage>
+          
+          </C.ViewImage>
+          
+          <C.Button>
+            <C.TitleButton>ENVIAR</C.TitleButton>
+          </C.Button>
+        </C.Info>
+      </KeyboardAvoidingView>
+    </C.Container>
   );
 };
 
---
+++ front/src/Pages/New/style.js
@@ -0,0 +1,80 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView``;
+
+export const SubHeader = styled.View`
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+`;
+
+export const Title = styled.Text`
+    margin-bottom: 15px;
+    font-size: 25px;
+    font-weight: bold;
+    color: #1565C0;
+`;
+
+export const SubTitle = styled.Text`
+    font-size: 12px;
+`;
+
+export const Info = styled.View`
+    align-items: center;
+    justify-content: center;
+    margin-top: 25px;
+`;
+
+export const InfoTitle = styled.Text`
+    font-weight: bold;
+    margin-bottom: 10px;
+`;
+
+export const InputMenor = styled.TextInput`
+    border: 1px solid #1565C0;
+    border-radius: 15px;
+    width: 85%;
+    height: 48px;
+    padding-left: 10px;
+    margin-bottom: 20px;
+`;
+
+export const InputDesc = styled.TextInput`
+    border: 1px solid #1565C0;
+    border-radius: 15px;
+    height: 108px;
+    width: 85%;
+    padding-left: 10px;
+    margin-bottom: 20px;
+`;
+
+export const ViewImage = styled.View`
+   align-items: center;
+   justify-content: center;
+   
+`;
+
+export const ButtonImage = styled.TouchableOpacity`
+    flex-direction: row;
+    border: 1px solid #1565C0;
+    border-radius: 15px;
+    height: 48px;
+    padding: 10px;
+    width: 340px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    background-color: #1565C0;
+    width: 85%;
+    margin-top: 30px;
+    align-items: center;
+    justify-content: center;
+    border-radius: 15px;
+    height: 48px;
+`;
+
+export const TitleButton = styled.Text`
+    font-size: 20px;
+    color: #FFF;
+    
+`;
