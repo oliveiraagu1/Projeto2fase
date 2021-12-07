851811558650c0e9d1094a89be25a8c7fe0b3844
julia <julia.silva@penseavanti.com.br>
2021-12-07 02:30:48 -0300

    tela de login arrumada

--- front/src/Pages/Login/style.js
+++ front/src/Pages/Login/style.js
@@ -2,36 +2,33 @@ import styled from "styled-components/native";
 
 export const Container = styled.SafeAreaView`
     flex: 1px;
+    position: relative;
     background-color: #fff;
 `;
-
 export const Title = styled.Text`
     font-weight: bold;
     font-size: 35px;
-    color: #2E9AFE;
-    margin-bottom: 20px;
+    color: #1E88E5;
     margin-left: 8px;
-
+    position: absolute;
+    top: -10%;
 `;
-
-export const EsqueciSenha = styled.TouchableOpacity`
-    margin-left: 200px;
-    margin-top: 10px;
-`;
-
+export const ContainerInput = styled.View `
+    height: 45%;
+    margin-top: 20px;
+`
 export const Login = styled.View`
     background-color: #FFF;
     flex: 1px;
 `;
-
 export const ImageTop = styled.Image`
     width: 100%;
-    height: 40%;
+    height: 49%;
 `;
 
 export const ImageBot = styled.Image`
     width: 100%;
-    height: 40%;
+    height: 50%;
 `;
 
 export const Text = styled.Text`
